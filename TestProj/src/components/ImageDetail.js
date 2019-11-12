f8c6c9bd457d04721887eca0af73883b5258210c
Abrahm Coury <apcoury@asu.edu>
2019-11-12 14:03:48 -0700

    First six sections of course

--- TestProj/src/components/ImageDetail.js
+++ TestProj/src/components/ImageDetail.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
 
-const ImageDetail = props => {
-    console.log(props);
-
+const ImageDetail = ({imageSource, title}) => {
     return (
         <View>
-            <Image source={props.imageSource} />
-            <Text>{props.title}</Text>
+            <Image
+                align="center"
+                style={{width: 100, height: 100}}
+                source={imageSource}
+            />
+            <Text>{title}</Text>
         </View>
     );
 };
--- TestProj/src/screens/HomeScreen.js
+++ TestProj/src/screens/HomeScreen.js
@@ -18,6 +18,22 @@ const HomeScreen = ({navigation}) => {
         title="Go to Image"
         onPress={() => navigation.navigate('Image')}
       />
+      <Button
+        title="Go to Counter"
+        onPress={() => navigation.navigate('Counter')}
+      />
+      <Button
+        title="Go to Color"
+        onPress={() => navigation.navigate('Color')}
+      />
+      <Button
+        title="Go to Square"
+        onPress={() => navigation.navigate('Square')}
+      />
+      <Button
+        title="Go to Text"
+        onPress={() => navigation.navigate('Text')}
+      />
     </View>
   );
 
